8259d2abbb33debf6a12c51d182cca59ad3cbef7
LifeIsFiction5 <jacob_barrett@icloud.com>
2026-02-05 23:38:12 -0600

    remove CSS import from Mission Page

--- src/app/Mission/page.jsx
+++ src/app/Mission/page.jsx
@@ -1,14 +1,12 @@
-import styles from "./MissionPage.module.css";
 import SiteHeader from "../../components/SiteHeader";
 import Footer from "../../components/Footer";
-import LogoMark from "../../components/LogoMark";
 import MissionHub from "../../components/MissionHub";
 
 export default function MissionPage() {
     return (
         <>
         <SiteHeader />
-        <main className={styles.main}>
+        <main>
             <MissionHub />
         </main>
         <Footer />
